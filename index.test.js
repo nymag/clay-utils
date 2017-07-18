f4c2be93921964b64f5a21cc1836fe220db35305
Chris Kirk <cperryk@gmail.com>
2017-07-18 14:23:01 -0400

    Removes dynamic requires

--- index.test.js
+++ index.test.js
@@ -2,40 +2,18 @@
 
 const _ = require('lodash'),
   filename = __filename.split('/').pop().split('.').shift(),
-  nymagfs = require('nymag-fs'),
   expect = require('chai').expect,
-  sinon = require('sinon'),
-  lib = require('./' + filename);
+  lib = require('./' + filename),
+  fs = require('fs');
 
 
 describe(_.startCase(filename), function () {
-  let req, sandbox;
 
-  beforeEach(function () {
-    sandbox = sinon.sandbox.create();
-
-    // require shouldn't be called dynamically, but here we go
-    req = sandbox.stub();
-    req.resolve = sandbox.stub();
-    lib.setRequire(req);
-  });
-
-  afterEach(function () {
-    sandbox.restore();
-    lib.setRequire(require);
-  });
-
-  describe('requireUtils', function () {
-    const fn = lib[this.title],
-      fakeArr = ['filea', 'fileb', 'filec'];
-
-    it('requires each file in the array returned by getFolders', function () {
-      sandbox.stub(nymagfs, 'getFolders').returns(fakeArr);
-      req.returns('value');
-      fn();
-
-      expect(req.callCount).to.equal(fakeArr.length);
-      expect(lib['filea']).to.equal('value');
-    });
+  it('exports each util in lib', function () {
+    fs.readdirSync('./lib')
+      .filter(file => fs.statSync('./lib/' + file).isDirectory())
+      .forEach((util) => {
+        expect(lib).to.have.property(util);
+      });
   });
 });
